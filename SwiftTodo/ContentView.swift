dd49bd53561a4c4d680bfe0df7152ad00c27be60
Elfana, A | Elfaael <elfaael12@gmail.com>
2024-03-30 10:06:11 +0700

    feat: delete function

--- SwiftTodo/ContentView.swift
+++ SwiftTodo/ContentView.swift
@@ -28,10 +28,13 @@ struct ContentView: View {
                     }
                     
                 }
+                .onDelete(perform: deleteItems)
             }
             .navigationTitle("Todo")
             .navigationBarTitleDisplayMode(.inline)
-            .navigationBarItems(trailing:
+            .navigationBarItems(
+            leading: EditButton(),
+            trailing:
             Button(action: {
                 self.showingAddTodoView.toggle()
             },
@@ -71,37 +74,18 @@ struct ContentView: View {
         //        }
     }
     
-    //    private func addItem() {
-    //        withAnimation {
-    //            let newItem = Item(context: viewContext)
-    //            newItem.timestamp = Date()
-    //
-    //            do {
-    //                try viewContext.save()
-    //            } catch {
-    //                // Replace this implementation with code to handle the error appropriately.
-    //                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-    //                let nsError = error as NSError
-    //                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-    //            }
-    //        }
-    //    }
-    //
-    //    private func deleteItems(offsets: IndexSet) {
-    //        withAnimation {
-    //            offsets.map { items[$0] }.forEach(viewContext.delete)
-    //
-    //            do {
-    //                try viewContext.save()
-    //            } catch {
-    //                // Replace this implementation with code to handle the error appropriately.
-    //                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-    //                let nsError = error as NSError
-    //                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-    //            }
-    //        }
-    //    }
-    //}
+        private func deleteItems(offsets: IndexSet) {
+            for index in offsets {
+                let todo = todos[index]
+                viewContext.delete(todo)
+                do {
+                    try viewContext.save()
+                } catch {
+                    print(error)
+                }
+            }
+        }
+    
     //
     //private let itemFormatter: DateFormatter = {
     //    let formatter = DateFormatter()
